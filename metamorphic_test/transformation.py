afee6739685d2c024afcc9f71b20114d9aacbc24
Paul Schwind <pal03377@gmail.com>
2022-07-07 00:45:05 +0200

    add wraps again

--- metamorphic_test/transformation.py
+++ metamorphic_test/transformation.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 from typing import Generic, List, Literal, TypeVar, Any, Dict
-# from functools import wraps
+from functools import wraps
 
 
 B = TypeVar('B')
@@ -80,7 +80,7 @@ def transform(t: MetamorphicTransformation[F, T]):
     Usage: @transform(shift(1))
     """
     def decorator(fun):
-        # @wraps(fun)
+        @wraps(fun)
         def transformed(anchor: F, *args, **kwargs) -> T:
             transformed = t.transform(anchor, *args, **kwargs)
             return fun(anchor, transformed)
