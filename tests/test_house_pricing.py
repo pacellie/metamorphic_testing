7c08ad9270cfbda4d2c14dcb78f4a6983af17e7f
Paul Schwind <pal03377@gmail.com>
2022-07-06 21:19:49 +0200

    remove code that's not really used

--- tests/test_house_pricing.py
+++ tests/test_house_pricing.py
@@ -68,18 +68,14 @@ housing_num = housing.drop("ocean_proximity", axis=1)
 rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
 
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
-        self.add_bedrooms_per_room = add_bedrooms_per_room
     def fit(self, *_):
         return self  # nothing else to do
     def transform(self, X):
         rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
         population_per_household = X[:, population_ix] / X[:, households_ix]
-        if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, population_per_household,
-                         bedrooms_per_room]
-        return np.c_[X, rooms_per_household, population_per_household]
+        bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+        return np.c_[X, rooms_per_household, population_per_household,
+                        bedrooms_per_room]
 
 
 num_pipeline = Pipeline([
