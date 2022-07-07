86bc2495399bff10ce58c465e7e4cf84d8bdb7ea
Iris <irisritalu@gmail.com>
2022-07-08 00:32:51 +0200

    try to fix pytest again

--- metamorphic_test/suite.py
+++ metamorphic_test/suite.py
@@ -6,5 +6,5 @@ from typing import Callable, Any, Tuple, Optional, List
 class Suite:
     name: Optional[str] = None
     transformation: Optional[Callable[[Any], Any]] = None
-    parametrized: list[Tuple[str, List]] = field(default_factory=list)
+    parametrized: List[Tuple[str, List]] = field(default_factory=list)
     relation: Optional[Callable[[Any, Any], bool]] = None
