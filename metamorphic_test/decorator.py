e52998122e5c805dad1454e833f4afbcb277234f
Iris <irisritalu@gmail.com>
2022-07-09 12:17:24 +0200

    add module name to be able to run multiple test files

--- metamorphic_test/decorator.py
+++ metamorphic_test/decorator.py
@@ -1,28 +1,38 @@
+import inspect
 from collections import defaultdict
+from functools import wraps
+
 from .suite import Suite
 
+suites: defaultdict = defaultdict(lambda: defaultdict(Suite))
 
-suites: defaultdict = defaultdict(Suite)
+
+def module(func):
+    return inspect.getmodule(func)
 
 
 def name(custom_name):
     def wrapper(transform):
-        suites[transform.__name__].name = custom_name
+        suites[module(transform)][transform.__name__].name = custom_name
         return transform
+
     return wrapper
 
 
 def parametrized(arg, generator):
     def wrapper(transform):
+        @wraps(transform)
         def parametrized_transform(*args, **kwargs):
             kwargs[arg] = generator() if callable(generator) else generator
             return transform(*args, **kwargs)
+
         return parametrized_transform
+
     return wrapper
 
 
 def transformation(transform):
-    suites[transform.__name__].transform = transform
+    suites[module(transform)][transform.__name__].transform = transform
     return transform
 
 
@@ -30,9 +40,9 @@ def relation(transform):
     def wrapper(relation):
         found_transform = False
 
-        for suite in suites:
-            if suites[suite].transform == transform:
-                suites[suite].relation = relation
+        for suite in suites[module(relation)]:
+            if suites[module(relation)][suite].transform == transform:
+                suites[module(relation)][suite].relation = relation
                 found_transform = True
 
         if not found_transform:
@@ -41,20 +51,26 @@ def relation(transform):
                             f"for the relation {relation.__name__}")
 
         return relation
+
     return wrapper
 
 
 def metamorphic(sut):
     def execute(x):
-        for suite in suites:
-            transform = suites[suite].transform
-            relation = suites[suite].relation
+        print(f"{suites=}")
+        for suite in suites[module(sut)]:
+            transform = suites[module(sut)][suite].transform
+            relation = suites[module(sut)][suite].relation
 
-            name = suites[suite].name if suites[suite].name else suite
+            name = suites[module(sut)][suite].name \
+                if suites[module(sut)][suite].name \
+                else suite
+
             print(f"[running suite '{name}']"
                   f"\n\tinput: {x} "
                   f"\n\ttransform: {transform.__name__} "
                   f"\n\trelation: {relation.__name__}")
 
             assert relation(sut(x), sut(transform(x)))
+
     return execute
