ae41b106ba7ec8dea379bdcb11e4bb29a8240af1
Iris <irisritalu@gmail.com>
2022-07-13 22:56:13 +0200

    made the code more object-oriented

--- metamorphic_test/decorator.py
+++ metamorphic_test/decorator.py
@@ -1,36 +1,32 @@
-from collections import defaultdict
-from functools import wraps, partial
-from .metamorphic import MetamorphicTest
+from functools import wraps
 import inspect
 from .transforms import identity
 from .relations import equality
+from .suite import Suite
 
-suites: defaultdict = defaultdict(lambda: defaultdict(MetamorphicTest))
+suite = Suite()
 
 
 def metamorphic(name, *, transform=identity, relation=equality):
-    suite = MetamorphicTest(name=name, transforms=[(transform, 0)], relation=relation)
-    frame = inspect.stack()[1]
-    module = inspect.getmodule(frame[0])
-    suites[module.__name__][name] = suite
+    suite.metamorphic(name, transform=transform, relation=relation)
     return name
 
 
 def randomized(arg, generator):
     def wrapper(transform):
         @wraps(transform)
-        def parametrized_transform(*args, **kwargs):
+        def randomized_transform(*args, **kwargs):
             kwargs[arg] = generator() if callable(generator) else generator
             return transform(*args, **kwargs)
 
-        return parametrized_transform
+        return randomized_transform
 
     return wrapper
 
 
 def transformation(name, *, priority=0):
     def wrapper(transform):
-        suites[transform.__module__][name].transforms.append((transform, priority))
+        suite.transformation(name, transform, priority=priority)
         return transform
 
     return wrapper
@@ -39,7 +35,7 @@ def transformation(name, *, priority=0):
 def relation(*names):
     def wrapper(relation):
         for name in names:
-            suites[relation.__module__][name].relation = relation
+            suite.relation(name, relation)
         return relation
 
     return wrapper
@@ -48,11 +44,7 @@ def relation(*names):
 def system(flag=None, *, name=None):
     def wrapper(test):
         def execute(*args):
-            for suite in suites[test.__module__]:
-                if name and suite != name:
-                    continue
-
-                suites[test.__module__][suite].execute(test, *args)
+            suite.execute(name, test, *args)
 
         # Creates a fake function that calls to the 'execute' function
         # with the exactly same signature as 'test'.
