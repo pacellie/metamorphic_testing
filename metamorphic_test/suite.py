84520cc51eaca10a26650d7a93e8d62ef2cabd39
Paul Schwind <paul.schwind@tum.de>
2022-07-15 00:08:37 +0200

    nicer test ID handling

--- metamorphic_test/suite.py
+++ metamorphic_test/suite.py
@@ -1,6 +1,7 @@
 import inspect
 from collections import defaultdict
 from functools import wraps
+from types import FunctionType
 
 from .metamorphic import MetamorphicTest
 
@@ -32,17 +33,25 @@ class Suite:
 
         self.tests[f"{module}.{name}"] = test
 
+    def _get_test(self, function_in_module: FunctionType, name: str) -> MetamorphicTest:
+        """Get the metamorphic test for the module of the given function and name."""
+        return self.tests[f"{function_in_module.__module__}.{name}"]
+
     def transformation(self, name, transform, *, priority):
-        self.tests[f"{transform.__module__}.{name}"].transforms.append((transform, priority))
+        self._get_test(transform, name).transforms.append((transform, priority))
 
     def relation(self, name, relation):
-        self.tests[f"{relation.__module__}.{name}"].relation = relation
+        self._get_test(relation, name).relation = relation
 
     def execute(self, name, test_function, *args):
         assert name is not None
-        self.tests[f"{test_function.__module__}.{name}"].execute(test_function, *args)
+        self._get_test(test_function, name).execute(test_function, *args)
+
+    def _belongs_to(self, test_id: str, module_name: str) -> bool:
+        """Check if the given test belongs to the given module."""
+        return test_id.startswith(f"{module_name}.")
 
     def execute_all(self, test_function, *args):
         for full_name, m_test in self.tests.items():
-            if full_name.startswith(f"{test_function.__module__}."):
+            if self._belongs_to(full_name, test_function.__module__):
                 m_test.execute(test_function, *args)
