7f769ce08f1af0a56092c4944b4ae6b884a1b98c
Paul Schwind <paul.schwind@tum.de>
2022-07-14 22:13:48 +0200

    first working house price prediction example in the new framework and with refactored prediction code

--- metamorphic_test/relations.py
+++ metamorphic_test/relations.py
@@ -6,3 +6,15 @@ def approximately(x, y):
 
 def equality(x, y):
     return x == y
+
+def becomes_larger(x, y):
+    return x < y
+
+def becomes_smaller(x, y):
+    return x > y
+
+def or_(rel1, rel2):
+    def or_impl(x, y):
+        return rel1(x, y) or rel2(x, y)
+    or_impl.__name__ = f'{rel1.__name__} or {rel2.__name__}'
+    return or_impl
---
+++ tests/house_pricing/test_house_pricing.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pytest
+
+from metamorphic_test import (
+    transformation,
+    metamorphic,
+    system,
+    randomized,
+    randint,
+)
+from metamorphic_test.relations import becomes_larger, approximately, or_
+
+from tests.house_pricing.house_pricing import (  # type: ignore
+    HousingPricePredictor,
+    get_housing_data,
+    get_training_and_test_set,
+    hide_labels,
+)
+
+
+housing = get_housing_data()
+training_set, test_set = get_training_and_test_set(get_housing_data())
+p = HousingPricePredictor(training_set)
+hide_labels(test_set)  # otherwise this MT would be somewhat pointless
+
+HousePriceTest = metamorphic(
+    'HousePriceTest', 
+    relation=or_(approximately, becomes_larger)
+)
+
+@transformation(HousePriceTest)
+@randomized('increase_rooms_by', randint(1, 10))
+def transform(x, increase_rooms_by: int) -> np.ndarray:
+    assert all(x["total_rooms"] % 1 == 0)
+    copy = x.copy()
+    copy["total_rooms"] += increase_rooms_by
+    return copy
+
+
+@pytest.mark.parametrize(
+    'x',
+    [test_set.iloc[n:n+1] for n in range(20)],
+)
+@system(name=HousePriceTest)
+def test_house_pricing_more_rooms(x) -> float:
+    assert all(x["total_rooms"] % 1 == 0)
+    return p.predict(x)
