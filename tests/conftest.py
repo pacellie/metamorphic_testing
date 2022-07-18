4409840dc262aa981f6f7fa1c9d35ce6765ded13
Martin Rau <martin.rau@tum.de>
2022-07-18 19:18:34 +0200

    delete conftest.py

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-from metamorphic_test import suite
-
-def pytest_generate_tests(metafunc):
-    if "meta" in metafunc.fixturenames:
-        module = metafunc.function.__module__
-        metafunc.parametrize("meta", suite.get_tests(module))
