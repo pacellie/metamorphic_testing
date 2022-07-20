6710b0043e3a4d76ce87e86cc40f7025822bd53b
Martin Rau <martin.rau@tum.de>
2022-07-20 16:41:31 +0200

    update system decorator

--- metamorphic_test/helper.py
+++ metamorphic_test/helper.py
@@ -1,9 +1,13 @@
 import inspect
+from sqlite3 import adapt
 import wrapt  # type: ignore
 
 
 def change_signature(adapt_func):
-    @wrapt.decorator(adapter=inspect.getfullargspec(adapt_func))
+    fullargspec = inspect.getfullargspec(adapt_func)
+    fullargspec.args.insert(0, 'name')
+
+    @wrapt.decorator(adapter=fullargspec)
     def change(wrapped, instance, args, kwargs):
         if instance is None:
             return wrapped(*args, **kwargs)
