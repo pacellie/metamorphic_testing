ab58eed76bbc09343b2a3738261c4df7b4a7ec0e
Martin Rau <martin.rau@tum.de>
2022-07-21 19:50:21 +0200

    type generators

--- metamorphic_test/generators/randint.py
+++ metamorphic_test/generators/randint.py
@@ -3,9 +3,10 @@ import random
 from metamorphic_test.generator import MetamorphicGenerator
 
 
-class RandInt(MetamorphicGenerator):
-    def __init__(self, min_value, max_value):
+class RandInt(MetamorphicGenerator[int]):
+    def __init__(self, min_value: int, max_value: int) -> None:
         self.min_value = min_value
         self.max_value = max_value
-    def generate(self):
+
+    def generate(self) -> int:
         return random.randint(self.min_value, self.max_value)  # nosec
