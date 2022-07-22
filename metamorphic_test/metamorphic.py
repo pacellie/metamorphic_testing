9ee3cbb774b1037d90535335a4d39ac305c86f0d
Tathagata Bandyopadhyay <tathagata.banerjee15@rocketmail.com>
2022-07-22 13:03:52 +0200

    adding docstrings for metamorphic.py, issue#65

--- metamorphic_test/metamorphic.py
+++ metamorphic_test/metamorphic.py
@@ -13,6 +13,35 @@ Relation = Callable[[Any, Any], bool]
 
 @dataclass
 class MetamorphicTest:
+    """
+    This class holds a single metamorphic test object along with its associated
+    transformations and relation and provides a mechanism to execute the test.
+
+    Attributes
+    ----------
+    name : str
+        name of the metamorphic test. This is optional but rally useful when there
+        are multiple metamorphic tests or some relation or transformation needs to
+        be added to the test at later point of time after itS creation.
+
+    transformation : List[PrioritizedTransform]
+        a list of transformations to be applied on the source input to perform the
+        metamorphic test.
+
+    relation : Optional[Relation]
+        a function that compares the source test case output to followup test case
+        output and returns True if the relation holds, False otherwise.
+
+    reports : List[MetamorphicExecutionReport]
+        a list of objects of MetamorphicExecutionReport which hold the reports generation
+        and logging logics for corresponding metamorphic tests.
+
+    See Also
+    --------
+    decorator.metamorphic : Registers a new metamorphic test
+    suite.Suite.metamorphic : This method is internally called by decorator.metamorphic()
+                            to register a metamorphic test
+    """
     name: Optional[str] = None
     transforms: List[PrioritizedTransform] = field(
         default_factory=lambda: []
@@ -23,9 +52,45 @@ class MetamorphicTest:
     )
 
     def add_transform(self, transform, priority=0):
+        """
+        Registers a transformation to a metamorphic test object
+
+        Parameters
+        ----------
+        transform : callable
+            a function (or any callable) to be used as a transformation
+            for a particular metamorphic test. Ideally, this refers to
+            the function decorated by decorator.transformation
+
+        priority : int
+            Optional keyword argument to set the order of the particular
+            transformation of interest in a multiple transformation chain
+            scenario. Default: 0
+
+        See Also
+        --------
+        decorator.transformation : registers a decorated function as a
+                                   transformation for a metamorphic test
+        suite.Suite.add_transform : registers a function as a transformation
+                                  to a metamorphic test
+        """
         self.transforms.append(PrioritizedTransform(transform, priority))
 
     def set_relation(self, relation):
+        """
+        Registers a relation to a metamorphic test object
+
+        Parameters
+        ----------
+        relation : Relation
+            a function that compares the source test case output to followup test
+            case output and returns True if the relation holds, False otherwise.
+
+        See Also
+        --------
+        decorator.relation : registers a function as a relation for a metamorphic test
+        suite.Suite.set_relation : registers a function as a relation to a metamorphic test
+        """
         if self.relation:
             raise ValueError(f"Relation to {self.name} already set ({self.relation}).")
         self.relation = relation
@@ -42,6 +107,28 @@ class MetamorphicTest:
     # (4) print some logging information
     # (5) apply the system under test and assert the relation function
     def execute(self, system, *x):
+        """
+        Executes the metamorphic test defined in the object and generate
+        reports
+
+        Note: This method is internally called from suite.Suite.execute
+              with corresponding test_id
+
+        Parameters
+        ----------
+        system : callable
+            a function (or callable) which needs to be tested. This refers to the
+            function decorated with decorator.system
+
+        x : Any
+            actual inputs for the system under test
+
+        See Also
+        --------
+        decorator.system : Identifies the function decorated with this decorator as
+                           a SystemUnderTest and executes all the metamorphic tests
+        suite.Suite.execute : Execute a metamorphic test on a system under test
+        """
         if not self.relation:
             raise ValueError(
                 f"No relation registered on {self.name}, cannot execute test."
