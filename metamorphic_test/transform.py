b2ab9f6761192e94b699ea2ccb3d847adaaf9021
Martin Rau <martin.rau@tum.de>
2022-07-29 17:34:55 +0200

    add comment to transform.py

--- metamorphic_test/transform.py
+++ metamorphic_test/transform.py
@@ -1,3 +1,5 @@
 from typing import Callable
 
+# The general type of a transformation. Since it can have an arbitrary number
+# of arbitrary inputs and an arbitrary output, 'Callable' seems most appropriate.
 Transform = Callable
