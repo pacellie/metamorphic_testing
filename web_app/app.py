c00486387ee4c52863e13a4c9a3b542ede9376d0
Tathagata Bandyopadhyay <tathagata.banerjee15@rocketmail.com>
2022-07-29 13:02:36 +0200

    separate asset and static, issue #123

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from argparse import ArgumentParser
 import glob
 import os
@@ -16,21 +16,25 @@ def index():
                            module_list=["all"] + [d.split(os.sep)[-2] for d in dirs])
 
 
+# Custom static data
+@app.route('/cdn/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(app.config['CUSTOM_STATIC_PATH'], filename, conditional=True)
+
+
 @app.route("/test", methods=["POST"])
 def run_test():
     cwd = os.getcwd()
     if cwd.split(os.sep)[-1] == "web_app":
         os.chdir("..")
     dirs = glob.glob(f"{args.test_directory}/*/")
-    # report_name = "reports/audio_test_new.html"
-    # print(f"Check box value: {request.form.get('check_box')}")
     report_name = request.form.get("report_name")
 
     selected_module = request.form.get("modules")
     report_name = report_name.replace(".html", "")+".html" if report_name \
         else f"report_{selected_module}_webapp.html"
     if not request.form.get('check_box'):
-        command_args = f" --html=web_app/static/reports/{report_name} --self-contained-html"
+        command_args = f" --html=assets/reports/{report_name} --self-contained-html"
         command = f"poetry run pytest {args.test_directory}" if selected_module == "all" \
             else f"poetry run pytest {args.test_directory}/{selected_module}"
         os.system(command+command_args)  # nosec
@@ -44,6 +48,7 @@ def run_test():
 
 
 if __name__ == "__main__":
+    app.config['CUSTOM_STATIC_PATH'] = os.path.join(app.root_path, '../assets/')
     parser = ArgumentParser()
     parser.add_argument("--test_directory", "-t",
                         type=str, default="examples",
