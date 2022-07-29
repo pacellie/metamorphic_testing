cfe978b5c63f6f896b8255deef9705455353d376
Martin Rau <martin.rau@tum.de>
2022-07-29 17:33:10 +0200

    make toplevel docstrings normal comments

--- metamorphic_test/rel.py
+++ metamorphic_test/rel.py
@@ -26,16 +26,14 @@ class Comparable(metaclass=ABCMeta):
         ...
 
 
-"""Common type variable for unconstrained relations."""
+# Common type variable for unconstrained relations.
 A = TypeVar('A')
-# any type which instantiates the type variable 'B' must support the common
-# comparison operators defined in the 'Comparable' class
-"""
-Common type variable for relations which contrain the type to support python's
-usual comparison operators. Also see the 'Comparable' ABC.
-"""
+
+
+# Common type variable for relations which contrain the type to support python's
+# usual comparison operators. Also see the 'Comparable' ABC.
 B = TypeVar('B', bound=Comparable)
 
 
-"""The general type of an un-constrained relation."""
+# The general type of an un-constrained relation.
 Relation = Callable[[A, A], bool]
