bebcc05231dbb544b38283e5f1c2938aa8840dc4
Iris <irisritalu@gmail.com>
2022-07-30 16:05:22 +0200

    added type and docstrings for helper.py

--- metamorphic_test/helper.py
+++ metamorphic_test/helper.py
@@ -1,8 +1,34 @@
 import inspect
 import wrapt  # type: ignore
+from typing import Callable
+
+SignatureWrapper = Callable[[Callable], Callable]
 
 
-def change_signature(adapt_func):
+def change_signature(adapt_func: Callable) -> SignatureWrapper:
+    """
+    Signature changing decorator.
+    Changing from the original function signature to adapt_func function's.
+
+    Parameters
+    ----------
+    adapt_func : Callable
+        The function with the argument specification as a prototype will be sourced.
+
+    Returns
+    -------
+    change : SignatureWrapper
+        Returns a function that its signature has been dynamically
+        changed with annotations based on the adapt_func function.
+
+    Examples
+    --------
+    def prototype(arg1, arg2): pass
+
+    @change_signature(prototype)
+    def function(input):
+        ...
+    """
     fullargspec = inspect.getfullargspec(adapt_func)
     fullargspec.args.insert(0, 'name')
 
