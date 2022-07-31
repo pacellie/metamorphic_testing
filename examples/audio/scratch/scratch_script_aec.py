aff19b9dc183f693c1fbc8a6eb669fb45399d3ef
Tathagata Bandyopadhyay <tathagata.banerjee15@rocketmail.com>
2022-07-31 22:55:00 +0200

    fixing variable name

--- examples/audio/scratch/scratch_script_aec.py
+++ examples/audio/scratch/scratch_script_aec.py
@@ -11,20 +11,20 @@ from examples.audio.models.aec_inference import AudioTagging, labels  # type: ig
 warnings.filterwarnings("ignore")
 
 
-def get_audio_tagging_result(clipwise_output, top_k=10):
+def get_audio_tagging_result(clip_wise_output, top_k=10):
     """Visualization of audio tagging result.
     Args:
-      clipwise_output: (classes_num,)
+      clip_wise_output: (classes_num,)
         output softmax distribution across all classes
       top_k: int
         number of top predictions to print
     """
-    sorted_indexes = np.argsort(clipwise_output)[::-1]
+    sorted_indexes = np.argsort(clip_wise_output)[::-1]
 
     # Print audio tagging top probabilities
     for k in range(top_k):
         print(f'{np.array(labels)[sorted_indexes[k]]}: '
-              f'{clipwise_output[sorted_indexes[k]]:.3f}')
+              f'{clip_wise_output[sorted_indexes[k]]:.3f}')
 
 
 if __name__ == "__main__":
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     # source input
     print('------ Audio tagging Source Input------')
     at = AudioTagging(checkpoint_path=None, device=DEVICE)
-    (clipwise_output, embedding) = at.inference(audio)
-    get_audio_tagging_result(np.squeeze(clipwise_output))
+    (clip_wise_output, embedding) = at.inference(audio)
+    get_audio_tagging_result(np.squeeze(clip_wise_output))
 
     # followup inputs
     augment = Compose([
@@ -50,5 +50,5 @@ if __name__ == "__main__":
     for i in range(N_AUG):
         print(f'\n------ Audio tagging Follow up Input: {i+1} ------')
         aug_audio = augment(audio, 32000).reshape((1, -1))
-        (clipwise_output_followup, embedding_followup) = at.inference(aug_audio)
-        get_audio_tagging_result(np.squeeze(clipwise_output_followup))
+        (clip_wise_output_followup, embedding_followup) = at.inference(aug_audio)
+        get_audio_tagging_result(np.squeeze(clip_wise_output_followup))
