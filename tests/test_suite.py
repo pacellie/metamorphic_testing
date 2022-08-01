7bfe5e91495bd48c8b85055f0af8cb78d83637dc
Iris <irisritalu@gmail.com>
2022-08-01 23:10:58 +0200

    set single name value

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -8,36 +8,36 @@ from metamorphic_test.suite import Suite
 from metamorphic_test.metamorphic import MetamorphicTest
 from metamorphic_test.transform import Transform
 
+NAME = 'a'
+
 
 @pytest.fixture(scope='module')
 def current_module_name():
     return inspect.getmodule(inspect.currentframe()).__name__
 
 
-@pytest.mark.parametrize('name', ['a'])
-def test_get_test(name):
+def test_get_test():
     suite = Suite()
 
     with pytest.raises(KeyError):
-        suite.get_test(name)
+        suite.get_test(NAME)
 
-    test_id = suite.metamorphic(name)
+    test_id = suite.metamorphic(NAME)
     test_object = suite.get_test(test_id)
     assert isinstance(test_object, MetamorphicTest)
 
 
-@pytest.mark.parametrize('name', ['a'])
-def test_get_test_id(name, current_module_name):
+def test_get_test_id(current_module_name):
     suite = Suite()
     assert not suite.get_test_id()
 
-    suite.metamorphic(name)
+    suite.metamorphic(NAME)
     test_id = suite.get_test_id()
     assert isinstance(test_id, tuple)
-    assert len(test_id) == len(name)
-    assert name in str(test_id)
+    assert len(test_id) == len(test_id)
+    assert NAME in str(test_id)
 
-    assert f"{current_module_name}.{name}" in str(test_id)
+    assert f"{current_module_name}.{NAME}" in str(test_id)
 
 
 def test_get_caller_module(current_module_name):
@@ -80,63 +80,59 @@ def test_randomized_generator(value, randomized):
     assert randomized_func(value) == (value, randomized)
 
 
-@pytest.mark.parametrize('name', ['a'])
-def test_metamorphic(name, current_module_name):
+def test_metamorphic(current_module_name):
     suite = Suite()
-    mock_test_id = suite.metamorphic(name)
+    mock_test_id = suite.metamorphic(NAME)
 
     assert isinstance(mock_test_id, str)
     assert current_module_name in mock_test_id
-    assert name in mock_test_id
-    assert f"{current_module_name}.{name}" == mock_test_id
+    assert NAME in mock_test_id
+    assert f"{current_module_name}.{NAME}" == mock_test_id
 
     with pytest.raises(ValueError):
-        suite.metamorphic(name)
+        suite.metamorphic(NAME)
 
 
-@pytest.mark.parametrize('test_id', ['a'])
 @given(priority=st.integers(0, 5))
-def test_add_transform(test_id, priority):
+def test_add_transform(priority):
     suite = Suite()
 
     def transform():
         pass
 
     with pytest.raises(KeyError):
-        suite.add_transform(test_id, transform)
-        suite.add_transform(test_id, transform, priority=priority)
+        suite.add_transform(NAME, transform)
+        suite.add_transform(NAME, transform, priority=priority)
 
-    test_id = suite.metamorphic(test_id)
+    test_id = suite.metamorphic(NAME)
     assert suite.add_transform(test_id, transform) is None
     assert suite.add_transform(test_id, transform, priority=priority) is None
 
 
-@pytest.mark.parametrize('test_id', ['a'])
-def test_set_relation(test_id):
+def test_set_relation():
     suite = Suite()
 
     def relation() -> bool:
         pass
 
     with pytest.raises(KeyError):
-        suite.set_relation(test_id, relation)
+        suite.set_relation(NAME, relation)
 
-    test_id = suite.metamorphic(test_id)
+    test_id = suite.metamorphic(NAME)
     assert suite.set_relation(test_id, relation) is None
 
 
-@pytest.mark.parametrize('test_id', ['a'])
 @given(args=st.tuples(st.integers(-5, 5), st.integers(-5, 5)))
-def test_execute(test_id, args):
+def test_execute(args):
     suite = Suite()
 
     def system():
         pass
 
     with pytest.raises(KeyError):
-        suite.execute(test_id, system, args)
+        suite.execute(NAME, system, args)
 
-    test_id = suite.metamorphic(test_id)
+    test_id = suite.metamorphic(NAME)
     with pytest.raises(ValueError):
         suite.execute(test_id, system, args)
 
